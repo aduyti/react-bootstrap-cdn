f7320c2261500eb16222f0b8484b0e800bed207f
Anirudhya <anirudhyaduyti@gmail.com>
2022-03-29 21:43:46 +0600

    cdn

---
+++ src/components/CardGroup/CardGroup.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Card from '../Card/Card';
+
+const CardGroup = () => {
+    return (
+        <div className="card-group">
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+        </div>
+    );
+};
+
+export default CardGroup;
